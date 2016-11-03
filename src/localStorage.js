3e1d14fa428c91d76e26f5793658727bab82b620
Sammi A <sammialldredge@gmail.com>
2016-11-02 22:13:33 -0700

    added local storage

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,14 +1,14 @@
 export function loadState() {
-  try{
-    let serializedState = localStorage.getItem('state');
-    if(!serializedState) throw new Error();
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (!serializedState) throw new Error();
     return JSON.parse(serializedState);
-  }catch (err){
+  } catch (err) {
     return undefined;
   }
 }
 
 export function saveState(state) {
-  let serializedState = JSON.stringify(state);
-  localStorage.setItem('state',serializedState);
+  const serializedState = JSON.stringify(state);
+  localStorage.setItem('state', serializedState);
 }
