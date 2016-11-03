94af971d89f3a31cfeb3624167c41751aad93de7
Donovan <donovan6925@gmail.com>
2016-11-02 23:57:07 -0700

    updated schemas and api routes

--- server/models/Company.js
+++ server/models/Company.js
@@ -4,8 +4,18 @@ const Schema = mongoose.Schema
 
 const companySchema = new Schema({
   name: { type: String, required: true },
+  companyInfo: { type: String },
+  logo: { type: String },
+  benefitInfo: { type: Array },
+  website: { type: String },
+  jobOpeningings: { type: Array },
+  tags: { type: Array },
   employees: [{ type: Schema.Types.ObjectId, ref: 'Employee' }],
 })
+
+companySchema.statics.getRatings = function(id) {
+  return this.findById(id).populate('employees');
+}
 
 const Company = mongoose.model('Company', companySchema);
 
